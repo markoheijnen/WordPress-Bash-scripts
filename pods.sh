c00ea29bbd8fd53171dca309d90c0a7b087171bd
Marko Heijnen <info@markoheijnen.com>
2013-05-01 20:21:15 +0200

    Change to another way to get DIR. Not perfect but hopefully now works with a cronjob

--- pods.sh
+++ pods.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+#DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR=$(dirname $0)
+
+cd $DIR
 
 if [ ! -d "$DIR/pods" ]; then
 	git clone git://github.com/pods-framework/pods.git
@@ -12,10 +15,10 @@ git pull 1>&1 | grep "Already up-to-date."
 
 
 mkdir -p $DIR/tmp/
-sh $DIR/i18n.sh wp-plugin $DIR/pods $DIR/tmp/pods.pot init
+bash $DIR/i18n.sh wp-plugin $DIR/pods $DIR/tmp/pods.pot init
 
 # GlotPress
 # "-p <project-path> -f <file> [-o <format>]"
 if [ ! -d "$DIR/../glotpress/scripts/import-originals.php" ]; then
-	php $DIR/../glotpress/scripts/import-originals.php -p pods-framework -f $DIR/tmp/pods.pot
+	php $DIR/../glotpress/scripts/import-originals.php -p pods-framework/dev -f $DIR/tmp/pods.pot
 fi
